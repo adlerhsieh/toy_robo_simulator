725fcfd7d4959748ea034f745ed0ed8f25c17d01
Adler <nkj20932@hotmail.com>
2016-11-10 00:45:24 +0800

    refactor duplicate code in validator_spec

--- spec/unit/validator_spec.rb
+++ spec/unit/validator_spec.rb
@@ -2,76 +2,68 @@ require 'spec_helper'
 require 'ostruct'
 
 describe ToyRoboSimulator::Validator do
-  let(:validator) { Class.new { include ToyRoboSimulator::Validator }.new }
-  before do
-    validator.instance_variable_set(:@errors, [])
-    validator.instance_variable_set(:@table, OpenStruct.new(x: 5, y: 5))
+  subject { Class.new { include ToyRoboSimulator::Validator }.new }
+  let(:errors) { subject.instance_variable_get(:@errors) }
+  
+  def set(key, value)
+    subject.instance_variable_set(key.to_sym, value)
   end
 
-  describe '#validate_placement' do
+  def place(x, y, orientation)
+    set :@x, x
+    set :@y, y
+    set :@orientation, orientation
+  end
+
+  before do
+    set :@errors, []
+    set :@table, OpenStruct.new(x: 5, y: 5)
+  end
+
+  describe '#valid_placement?' do
     it 'is valid' do
-      validator.validate_placement(1, 2, :north)
-      errors = validator.instance_variable_get(:@errors)
+      place(1, 2, :north)
+      expect(subject.valid_placement?).to be_truthy
       expect(errors).to be_empty
     end
     it 'with invalid type' do
-      validator.validate_placement('foo', 'bar', 'qux')
-      errors = validator.instance_variable_get(:@errors)
+      place('foo', 'bar', 'qux')
+      expect(subject.valid_placement?).to be_falsey
       expect(errors).not_to be_empty
       expect(errors).to include 'X must be a number'
       expect(errors).to include 'Y must be a number'
       expect(errors).to include 'Orientation should be either NORTH, SOUTH, EAST, or WEST'
     end
     it 'out of range' do
-      validator.validate_placement(8, 10, :north)
-      errors = validator.instance_variable_get(:@errors)
+      place(8, 10, :north)
+      expect(subject.valid_placement?).to be_falsey
       expect(errors).not_to be_empty
       expect(errors).to include 'X must be between 0 and 5'
       expect(errors).to include 'Y must be between 0 and 5'
     end
   end
 
-  describe '#validate_movement' do
+  describe '#moveable?' do
     it 'is valid' do
-      validator.instance_variable_set(:@x, 3)
-      validator.instance_variable_set(:@y, 3)
-      validator.instance_variable_set(:@orientation, :north)
-      validator.validate_movement
-      errors = validator.instance_variable_get(:@errors)
-      expect(errors).to be_empty
+      place(3, 3, :north)
+      expect(subject.moveable?).to be_truthy
     end
     describe 'is at edge of' do
       it 'north' do
-        validator.instance_variable_set(:@x, 3)
-        validator.instance_variable_set(:@y, 5)
-        validator.instance_variable_set(:@orientation, :north)
-        validator.validate_movement
-        errors = validator.instance_variable_get(:@errors)
-        expect(errors).not_to be_empty
+        place(3, 5, :north)
+        expect(subject.moveable?).to be_falsey
       end
       it 'east' do
-        validator.instance_variable_set(:@x, 5)
-        validator.instance_variable_set(:@y, 1)
-        validator.instance_variable_set(:@orientation, :east)
-        validator.validate_movement
-        errors = validator.instance_variable_get(:@errors)
-        expect(errors).not_to be_empty
+        place(5, 1, :east)
+        expect(subject.moveable?).to be_falsey
       end
       it 'south' do
-        validator.instance_variable_set(:@x, 0)
-        validator.instance_variable_set(:@y, 0)
-        validator.instance_variable_set(:@orientation, :south)
-        validator.validate_movement
-        errors = validator.instance_variable_get(:@errors)
-        expect(errors).not_to be_empty
+        place(0, 0, :south)
+        expect(subject.moveable?).to be_falsey
       end
       it 'west' do
-        validator.instance_variable_set(:@x, 0)
-        validator.instance_variable_set(:@y, 3)
-        validator.instance_variable_set(:@orientation, :west)
-        validator.validate_movement
-        errors = validator.instance_variable_get(:@errors)
-        expect(errors).not_to be_empty
+        place(0, 3, :west)
+        expect(subject.moveable?).to be_falsey
       end
     end
   end
